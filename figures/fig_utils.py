f0f313bd5f8c07870b6fe895cfed4d5cc4860de2
Urban Lutz <lutzurb1@students.zhaw.ch>
2022-06-06 21:55:12 +0000

    figs

--- figures/fig_utils.py
+++ figures/fig_utils.py
@@ -14,7 +14,36 @@ pretty_names = {
     "phase_0_strategy": "Scoring Criterion",
     "final_sparsity": "Sparsity",
     "accuracy": "Top-1 Accuracy",
+    "type_short": "Pruning Schedule",
 }
 
 def prettify_names(df):
+     
+
     return df.rename(columns=pretty_names)
+
+def replace_typeshort(df):
+    if "type_short" in df.columns:
+        df["type_short"] = df["type_short"].apply(lambda x: transform_typeshort(x))
+    return df
+
+def replace_algo(df):
+    if "phase_0_strategy" in df.columns:
+        
+        df["phase_0_strategy"] = df["phase_0_strategy"].apply(lambda x: transform_algos(x))
+    return df
+
+def transform_typeshort(s):
+    return s.replace("10", "MS").replace("1", "SS").replace("i", "I").replace("o", "U")
+
+def transform_algos(s):
+    nice_names = {
+            "snip": "SNIP",
+            "synflow": "SynFlow", 
+            "mag": "Magnitude",
+            "grasp": "GraSP",
+            "rand": "Random"
+    }
+    for k, v in nice_names.items():
+        s = s.replace(k, v)
+    return s
